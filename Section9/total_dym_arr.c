39d65779cb5db68b018cc434582c9339c36bacef
4n4k1n <anakin@pregitzer.de>
2024-12-24 13:06:53 +0100

    funktion for allocating matrix and free

--- Section9/total_dym_arr.c
+++ Section9/total_dym_arr.c
@@ -33,29 +33,29 @@ void free_2d_arr(void **arr, int height)
     free(arr);
 }
 
-int main (void)
-{
-    int height = 5;
-    int width = 5;
-    int **arr;
-    int i;
-    int j;
+// int main (void)
+// {
+//     int height = 5;
+//     int width = 5;
+//     int **arr;
+//     int i;
+//     int j;
 
-    arr = create_2d_arr(height, width);
-    if (!arr)
-        free_2d_arr((void **)arr, height);
-    i = 0;
-    while (i < height)
-    {
-        j = 0;
-        while (j < width)
-        {
-            printf("%d", arr[i][j]);
-            j++;
-        }
-        printf("\n");
-        i++;
-    }
-    free_2d_arr((void **)arr, height);
-    return (0);
-}
+//     arr = create_2d_arr(height, width);
+//     if (!arr)
+//         free_2d_arr((void **)arr, height);
+//     i = 0;
+//     while (i < height)
+//     {
+//         j = 0;
+//         while (j < width)
+//         {
+//             printf("%d", arr[i][j]);
+//             j++;
+//         }
+//         printf("\n");
+//         i++;
+//     }
+//     free_2d_arr((void **)arr, height);
+//     return (0);
+// }
---
+++ Section9/total_dym_arr.h
@@ -0,0 +1,7 @@
+#ifndef TOTAL_DYM_ARR_H
+#define TOTAL_DYM_ARR_H
+
+int **create_2d_arr(int height, int width);
+void free_2d_arr(void **arr, int height);
+
+#endif
